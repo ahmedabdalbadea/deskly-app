b3a61fa6bc7d2d93450d79f7f1b7610800c10020
Ahmed Abd Albadea <ahmedmohmaedabdalbadea@gmail.com>
2026-08-04 17:52:18 +0300

    add images of onboarding to app images

--- lib/core/utils/app_images.dart
+++ lib/core/utils/app_images.dart
@@ -1,3 +1,7 @@
 abstract class AppImages {
   static const desklyLogo = "assets/images/deskly.png";
+  static const workspaceIllustration =
+      "assets/images/workspace_illustration.png";
+  static const bookingIllustration = "assets/images/booking_illustration.png";
+  static const qrIllustration = "assets/images/qr_illustration.png";
 }
