a6b93a0c4562855a833c85297f028af6292ff654
Ahmed Abd Albadea <ahmedmohmaedabdalbadea@gmail.com>
2026-08-05 16:15:52 +0300

    add green and pink gradients to app gradients

--- lib/core/theme/app_gradients.dart
+++ lib/core/theme/app_gradients.dart
@@ -8,4 +8,16 @@ abstract final class AppGradients {
     end: Alignment.bottomRight,
     colors: [AppColors.primaryBlue, AppColors.primaryPurple],
   );
+
+  static const green = LinearGradient(
+    begin: Alignment.topLeft,
+    end: Alignment.bottomRight,
+    colors: [Color(0xFF22C55E), Color(0xFF16A34A)],
+  );
+
+  static const pink = LinearGradient(
+    begin: Alignment.topLeft,
+    end: Alignment.bottomRight,
+    colors: [Color(0xFFFF6B8A), Color(0xFFFF4D6D)],
+  );
 }
