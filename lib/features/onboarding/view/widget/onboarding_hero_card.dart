fc1012a068750d06cf310e69bd7d7945bf42801b
Ahmed Abd Albadea <ahmedmohmaedabdalbadea@gmail.com>
2026-08-06 14:54:40 +0300

    add animations to onboarding hero card

--- lib/features/onboarding/view/widget/onboarding_hero_card.dart
+++ lib/features/onboarding/view/widget/onboarding_hero_card.dart
@@ -1,6 +1,7 @@
+import 'package:deskly_app/constants.dart';
 import 'package:flutter/material.dart';
 
-class OnboardingHeroCard extends StatelessWidget {
+class OnboardingHeroCard extends StatefulWidget {
   const OnboardingHeroCard({
     super.key,
     required this.image,
@@ -8,29 +9,56 @@ class OnboardingHeroCard extends StatelessWidget {
   });
   final String image;
   final Gradient gradient;
+
+  @override
+  State<OnboardingHeroCard> createState() => _OnboardingHeroCardState();
+}
+
+class _OnboardingHeroCardState extends State<OnboardingHeroCard> {
+  double _circleAvatarPosition = -80;
+  double _imageScale = 0.97;
+  @override
+  void initState() {
+    super.initState();
+    _forwardAnimation();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
       decoration: BoxDecoration(
-        gradient: gradient,
+        gradient: widget.gradient,
         borderRadius: BorderRadius.circular(28),
       ),
       child: Stack(
         children: [
-          Positioned(
-            top: -40,
-            right: -40,
+          AnimatedPositioned(
+            duration: kAnimationDuration,
+            top: _circleAvatarPosition,
+            right: _circleAvatarPosition,
             child: CircleAvatar(backgroundColor: Colors.white12, radius: 80),
           ),
 
-          Positioned(
-            left: -40,
-            bottom: -40,
+          AnimatedPositioned(
+            duration: kAnimationDuration,
+            left: _circleAvatarPosition,
+            bottom: _circleAvatarPosition,
             child: CircleAvatar(backgroundColor: Colors.white12, radius: 80),
           ),
-          Image.asset(image, fit: BoxFit.fill),
+          AnimatedScale(
+            duration: kAnimationDuration,
+            scale: _imageScale,
+            child: Image.asset(widget.image, fit: BoxFit.fill),
+          ),
         ],
       ),
     );
+  }
+
+  Future<Null> _forwardAnimation() {
+    return Future.delayed(const Duration(milliseconds: 200), () {
+      _circleAvatarPosition = -40;
+      _imageScale = 1;
+    });
   }
 }
