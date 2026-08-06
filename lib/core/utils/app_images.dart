589a85a992f4feedf11d9b9b3d1ae7f28eb2559e
Ahmed Abd Albadea <ahmedmohmaedabdalbadea@gmail.com>
2026-08-06 19:05:31 +0300

    add logos and icons of auth feature to app images

--- lib/core/utils/app_images.dart
+++ lib/core/utils/app_images.dart
@@ -1,7 +1,20 @@
 abstract class AppImages {
   static const desklyLogo = "assets/images/deskly.png";
+  static const googleLogo = "assets/images/google_logo.svg";
+  static const facebookLogo = "assets/images/facebook_logo.svg";
+  static const linkedinLogo = "assets/images/linkedin_logo.svg";
+
   static const workspaceIllustration =
       "assets/images/workspace_illustration.png";
   static const bookingIllustration = "assets/images/booking_illustration.png";
   static const qrIllustration = "assets/images/qr_illustration.png";
+
+  static const emailIcon = "assets/images/email.svg";
+  static const lockIcon = "assets/images/lock.svg";
+  static const lockKeyIcon = "assets/images/lock_key.svg";
+  static const phoneIcon = "assets/images/phone.svg";
+  static const personIcon = "assets/images/person.svg";
+  static const eyeIcon = "assets/images/eye.svg";
+  static const eyeOffIcon = "assets/images/eye_off.svg";
+  static const lightBulbIcon = "assets/images/lightbulb.svg";
 }
