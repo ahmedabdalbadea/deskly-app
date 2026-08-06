e00ea76a50ae892875376e721522d604da8538e6
Ahmed Abd Albadea <ahmedmohmaedabdalbadea@gmail.com>
2026-08-06 17:56:03 +0300

    create login header

--- lib/features/auth/view/widget/login_view_body.dart
+++ lib/features/auth/view/widget/login_view_body.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 
+import 'login_header.dart';
+
 class LoginViewBody extends StatelessWidget {
   const LoginViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Column(children: [LoginHeader()]);
   }
 }
