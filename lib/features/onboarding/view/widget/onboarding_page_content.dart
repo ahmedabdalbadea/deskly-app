910ff075f83ef35495038d2684e09635de627f66
Ahmed Abd Albadea <ahmedmohmaedabdalbadea@gmail.com>
2026-08-06 15:38:14 +0300

    add animation to onboarding page content

--- lib/features/onboarding/view/widget/onboarding_page_content.dart
+++ lib/features/onboarding/view/widget/onboarding_page_content.dart
@@ -1,12 +1,29 @@
+import 'package:deskly_app/constants.dart';
 import 'package:deskly_app/core/theme/app_text_styles.dart';
 import 'package:deskly_app/features/onboarding/data/models/onboarding_model.dart';
 import 'package:flutter/material.dart';
 
 import 'onboarding_hero_card.dart';
 
-class OnboardingPageContent extends StatelessWidget {
+class OnboardingPageContent extends StatefulWidget {
   const OnboardingPageContent({super.key, required this.item});
   final OnboardingModel item;
+
+  @override
+  State<OnboardingPageContent> createState() => _OnboardingPageContentState();
+}
+
+class _OnboardingPageContentState extends State<OnboardingPageContent> {
+  double _titleOpacity = 0, _descriptionOpacity = 0;
+  Offset _titleOffset = const Offset(0, 0.12),
+      _descriptionOffset = const Offset(0, 0.12);
+
+  @override
+  void initState() {
+    super.initState();
+    _forwardAnimation();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -14,23 +31,58 @@ class OnboardingPageContent extends StatelessWidget {
       children: [
         AspectRatio(
           aspectRatio: 1,
-          child: OnboardingHeroCard(image: item.image, gradient: item.gradient),
+          child: OnboardingHeroCard(
+            image: widget.item.image,
+            gradient: widget.item.gradient,
+          ),
         ),
         const SizedBox(height: 36),
 
-        Text(
-          item.title,
-          style: AppTextStyles.extraBold28(context),
-          maxLines: 2,
+        AnimatedSlide(
+          duration: kAnimationDuration,
+          offset: _titleOffset,
+          child: AnimatedOpacity(
+            duration: kAnimationDuration,
+            opacity: _titleOpacity,
+            child: Text(
+              widget.item.title,
+              style: AppTextStyles.extraBold28(context),
+              maxLines: 2,
+            ),
+          ),
         ),
         const SizedBox(height: 12),
-        Text(
-          item.description,
-          style: AppTextStyles.regular15(
-            context,
-          ).copyWith(color: const Color(0xff6E6E8A)),
+        AnimatedSlide(
+          duration: kAnimationDuration,
+          offset: _descriptionOffset,
+          child: AnimatedOpacity(
+            duration: kAnimationDuration,
+            opacity: _descriptionOpacity,
+            child: Text(
+              widget.item.description,
+              style: AppTextStyles.regular15(
+                context,
+              ).copyWith(color: const Color(0xff6E6E8A)),
+            ),
+          ),
         ),
       ],
     );
+  }
+
+  void _forwardAnimation() {
+    Future.delayed(const Duration(milliseconds: 400), () {
+      setState(() {
+        _titleOpacity = 1;
+        _titleOffset = Offset(0, 0);
+      });
+    });
+
+    Future.delayed(const Duration(milliseconds: 800), () {
+      setState(() {
+        _descriptionOpacity = 1;
+        _descriptionOffset = Offset(0, 0);
+      });
+    });
   }
 }
