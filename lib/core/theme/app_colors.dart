974b737301e135b16004d6bd5779abf6918243e5
Ahmed Abd Albadea <ahmedmohmaedabdalbadea@gmail.com>
2026-08-08 16:19:00 +0300

    add border color to app colors and create social auth button

--- lib/core/theme/app_colors.dart
+++ lib/core/theme/app_colors.dart
@@ -4,4 +4,5 @@ abstract final class AppColors {
   static const primaryBlue = Color(0xFF4FACFE);
   static const primaryPurple = Color(0xFF6C47FF);
   static const mutedText = Color(0xff9090B0);
+  static const border = Color(0xffE4E4F0);
 }
