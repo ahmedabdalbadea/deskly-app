4847a1c1dae1460da7166b1729f5cde9c5cc33b0
Ahmed Abd Albadea <ahmedmohmaedabdalbadea@gmail.com>
2026-08-10 18:47:36 +0300

    configure Firebase and initialize app

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:deskly_app/core/utils/app_router.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const DesklyApp());
 }
 
