6bdadb1f90e781c42bafdd597c568316e2397cc1
Ahmed Abd Albadea <ahmedmohmaedabdalbadea@gmail.com>
2026-08-10 15:58:54 +0300

    validate login form

---
+++ lib/core/utils/form_validators.dart
@@ -0,0 +1,36 @@
+abstract class FormValidators {
+  static String? requiredFieldValidator(String? value) {
+    if (value?.isEmpty ?? true) {
+      return "Field is required";
+    } else {
+      return null;
+    }
+  }
+
+  static String? validateEmail(String? value) {
+    final regex = RegExp(r'^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$');
+    if (value == null || !regex.hasMatch(value)) {
+      return "Enter a valid email";
+    }
+    return null;
+  }
+
+  static String? validatePassword(String? value) {
+    if (value == null || value.length < 8) {
+      return "Password must be at least 8 characters";
+    }
+    return null;
+  }
+
+  static String? validateInternationalPhone(String? value) {
+    final regexPhone = RegExp(r'^\+?[0-9\s\-\(\)]{9,15}$');
+
+    if (value == null || value.isEmpty) {
+      return "Phone number is required";
+    } else if (!regexPhone.hasMatch(value)) {
+      return "Please enter a valid phone number";
+    }
+
+    return null;
+  }
+}
--- lib/features/auth/view/widget/login_form.dart
+++ lib/features/auth/view/widget/login_form.dart
@@ -2,18 +2,29 @@ import 'package:deskly_app/core/theme/app_colors.dart';
 import 'package:deskly_app/core/theme/app_text_styles.dart';
 import 'package:deskly_app/core/utils/app_images.dart';
 import 'package:deskly_app/core/utils/app_router.dart';
+import 'package:deskly_app/core/utils/form_validators.dart';
 import 'package:deskly_app/core/widget/pressable.dart';
 import 'package:deskly_app/features/auth/view/widget/gradient_button.dart';
 import 'package:deskly_app/features/auth/view/widget/user_input.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
-class LoginForm extends StatelessWidget {
+class LoginForm extends StatefulWidget {
   const LoginForm({super.key});
+
+  @override
+  State<LoginForm> createState() => _LoginFormState();
+}
+
+class _LoginFormState extends State<LoginForm> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  AutovalidateMode _autovalidateMode = AutovalidateMode.disabled;
 
   @override
   Widget build(BuildContext context) {
     return Form(
+      key: _formKey,
+      autovalidateMode: _autovalidateMode,
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
@@ -23,6 +34,7 @@ class LoginForm extends StatelessWidget {
             child: UserInput(
               prefixIcon: AppImages.emailIcon,
               hint: "ahmed@example.com",
+              validator: FormValidators.validateEmail,
             ),
           ),
           const SizedBox(height: 18),
@@ -33,6 +45,7 @@ class LoginForm extends StatelessWidget {
               prefixIcon: AppImages.lockIcon,
               hint: "• • • • • • • •",
               isPassword: true,
+              validator: FormValidators.validatePassword,
             ),
           ),
           const SizedBox(height: 18),
@@ -59,6 +72,15 @@ class LoginForm extends StatelessWidget {
             title: "Sign In",
             radius: 16,
             padding: EdgeInsets.symmetric(vertical: 16),
+            onPressed: () {
+              if (_formKey.currentState!.validate()) {
+                _formKey.currentState!.save();
+              } else {
+                setState(() {
+                  _autovalidateMode = AutovalidateMode.always;
+                });
+              }
+            },
           ),
         ],
       ),
