3fa8d0a3dd016a75dcae5d4c0f270b14f56bba7f
Ahmed Abd Albadea <ahmedmohmaedabdalbadea@gmail.com>
2026-08-11 16:11:59 +0300

    create register use case

---
+++ lib/features/auth/domain/use_case/register/register_params.dart
@@ -0,0 +1,13 @@
+class RegisterParams {
+  final String name;
+  final String email;
+  final String phone;
+  final String password;
+
+  const RegisterParams({
+    required this.name,
+    required this.email,
+    required this.phone,
+    required this.password,
+  });
+}
