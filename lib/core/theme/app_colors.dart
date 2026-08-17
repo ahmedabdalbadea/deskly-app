046c63774cce49f1679de90ae02c834a11716432
Ahmed Abd Albadea <ahmedmohmaedabdalbadea@gmail.com>
2026-08-17 17:45:25 +0300

    add warnign and success color and add users collection const

--- lib/core/theme/app_colors.dart
+++ lib/core/theme/app_colors.dart
@@ -7,4 +7,6 @@ abstract final class AppColors {
   static const border = Color(0xffE4E4F0);
   static const surface = Color(0xffF0EEFF);
   static const error = Color(0xFFDC2626);
+  static const warning = Color(0xFFF59E0B);
+  static const success = Color(0xFF16A34A);
 }
