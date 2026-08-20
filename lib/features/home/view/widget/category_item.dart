97b3154d74a3eab2cf58f6de57521fd0a76bbb89
Ahmed Abd Albadea <ahmedmohmaedabdalbadea@gmail.com>
2026-08-20 14:54:46 +0300

    create category item

---
+++ lib/features/home/view/widget/category_item.dart
@@ -0,0 +1,65 @@
+import 'package:deskly_app/constants.dart';
+import 'package:deskly_app/core/theme/app_colors.dart';
+import 'package:deskly_app/core/theme/app_gradients.dart';
+import 'package:deskly_app/core/theme/app_text_styles.dart';
+import 'package:deskly_app/core/widget/pressable.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class CategoryItem extends StatelessWidget {
+  const CategoryItem({
+    super.key,
+    required this.icon,
+    required this.label,
+    this.isSelected = false,
+    this.onPressed,
+  });
+
+  final String icon;
+  final String label;
+  final bool isSelected;
+  final VoidCallback? onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return Pressable(
+      onPressed: onPressed,
+      radius: 16,
+      child: AnimatedContainer(
+        height: 90,
+        width: 76,
+        duration: kAnimationDuration,
+        curve: Curves.easeOut,
+        padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 8),
+        decoration: BoxDecoration(
+          gradient: isSelected ? AppGradients.primary : null,
+          color: isSelected ? null : AppColors.surface,
+          borderRadius: BorderRadius.circular(16),
+          boxShadow: isSelected
+              ? [
+                  BoxShadow(
+                    color: Colors.black.withValues(alpha: 0.12),
+                    blurRadius: 12,
+                    offset: const Offset(0, 4),
+                  ),
+                ]
+              : [],
+        ),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            SvgPicture.asset(icon, width: 28, height: 28),
+            const SizedBox(height: 8),
+            Text(
+              label,
+              style: AppTextStyles.semiBold10(context).copyWith(
+                color: isSelected ? Colors.white : AppColors.primaryText,
+              ),
+              textAlign: TextAlign.center,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
