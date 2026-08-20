d09a903a40ac8edbba8856b7e322e1250b2b39a3
Ahmed Abd Albadea <ahmedmohmaedabdalbadea@gmail.com>
2026-08-20 18:16:11 +0300

    create popular workspaces

--- lib/features/home/view/widget/home_view_body.dart
+++ lib/features/home/view/widget/home_view_body.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 import 'categories.dart';
 import 'home_app_bar.dart';
+import 'popular_workspaces.dart';
 
 class HomeViewBody extends StatelessWidget {
   const HomeViewBody({super.key});
@@ -12,9 +13,12 @@ class HomeViewBody extends StatelessWidget {
       padding: EdgeInsets.symmetric(horizontal: 20),
       child: CustomScrollView(
         slivers: [
-          SliverToBoxAdapter(child: SizedBox(height: 8)),
           HomeAppBar(),
           SliverToBoxAdapter(child: Categories()),
+          SliverToBoxAdapter(child: SizedBox(height: 20)),
+          SliverToBoxAdapter(child: PopularWorkspaces()),
+
+          SliverToBoxAdapter(child: SizedBox(height: 20)),
         ],
       ),
     );
---
+++ lib/features/home/view/widget/popular_workspaces.dart
@@ -0,0 +1,39 @@
+import 'package:deskly_app/core/theme/app_colors.dart';
+import 'package:deskly_app/core/theme/app_text_styles.dart';
+import 'package:deskly_app/core/widget/pressable.dart';
+import 'package:flutter/material.dart';
+
+import 'popular_workspaces_list.dart';
+
+class PopularWorkspaces extends StatelessWidget {
+  const PopularWorkspaces({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text("Popular Workspaces", style: AppTextStyles.bold16(context)),
+            Pressable(
+              radius: 0,
+              highlightColor: Colors.transparent,
+              splashColor: Colors.transparent,
+              onPressed: () {},
+              child: Text(
+                "See All",
+                style: AppTextStyles.medium13(
+                  context,
+                ).copyWith(color: AppColors.primaryPurple),
+              ),
+            ),
+          ],
+        ),
+        const SizedBox(height: 14),
+
+        const PopularWorkspacesList(),
+      ],
+    );
+  }
+}
