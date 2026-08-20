268da464ed10407c77a733e59caa8e57a36fb0bd
Ahmed Abd Albadea <ahmedmohmaedabdalbadea@gmail.com>
2026-08-20 18:14:26 +0300

    create work space item details

---
+++ lib/features/home/view/widget/popular_workspaces_item_details.dart
@@ -0,0 +1,71 @@
+import 'package:deskly_app/core/theme/app_colors.dart';
+import 'package:deskly_app/core/theme/app_text_styles.dart';
+import 'package:flutter/material.dart';
+
+class PopularWorkspacesItemDetails extends StatelessWidget {
+  const PopularWorkspacesItemDetails({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 14),
+      child: Column(
+        children: [
+          Row(
+            children: [
+              Text("The Hub Cairo", style: AppTextStyles.bold15(context)),
+              const Spacer(),
+              Text(
+                "EGP 80/",
+                style: AppTextStyles.bold15(
+                  context,
+                ).copyWith(color: AppColors.primaryPurple),
+              ),
+              Text(
+                "hr",
+                style: AppTextStyles.regular11(
+                  context,
+                ).copyWith(color: AppColors.mutedText),
+              ),
+            ],
+          ),
+
+          Row(
+            children: [
+              Icon(
+                Icons.location_on_outlined,
+                size: 14,
+                color: AppColors.mutedPurple,
+              ),
+              const SizedBox(width: 5),
+              Text(
+                "Cairo, Egypt",
+                style: AppTextStyles.regular12(
+                  context,
+                ).copyWith(color: AppColors.mutedPurple),
+              ),
+              const SizedBox(width: 5),
+              Text(
+                "•",
+                style: AppTextStyles.regular12(
+                  context,
+                ).copyWith(color: AppColors.mutedPurple),
+              ),
+              const SizedBox(width: 5),
+              Icon(Icons.star, size: 14, color: const Color(0xffFBBF24)),
+              const SizedBox(width: 5),
+              Text("4.8", style: AppTextStyles.semiBold12(context)),
+              const SizedBox(width: 5),
+              Text(
+                "(124)",
+                style: AppTextStyles.regular12(
+                  context,
+                ).copyWith(color: AppColors.mutedPurple),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
