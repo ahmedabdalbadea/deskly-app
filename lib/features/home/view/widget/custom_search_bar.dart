78df45627e0ce1dbb3efa1ad0c87f04c4efac1ab
Ahmed Abd Albadea <ahmedmohmaedabdalbadea@gmail.com>
2026-08-30 17:52:36 +0300

    create custom search bar

---
+++ lib/features/home/view/widget/custom_search_bar.dart
@@ -0,0 +1,41 @@
+import 'package:deskly_app/core/theme/app_colors.dart';
+import 'package:deskly_app/core/theme/app_text_styles.dart';
+import 'package:flutter/material.dart';
+
+class CustomSearchBar extends StatelessWidget {
+  const CustomSearchBar({super.key, this.controller});
+  final TextEditingController? controller;
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(12),
+      child: TextField(
+        controller: controller,
+        textAlignVertical: TextAlignVertical.center,
+        style: AppTextStyles.regular13(
+          context,
+        ).copyWith(color: AppColors.primaryText),
+
+        decoration: InputDecoration(
+          filled: true,
+          fillColor: const Color(0xffF6F7FB),
+          hintText: "Search workspaces, locations...",
+          hintStyle: AppTextStyles.regular13(
+            context,
+          ).copyWith(color: AppColors.mutedText),
+          contentPadding: const EdgeInsets.symmetric(
+            horizontal: 14,
+            vertical: 12,
+          ),
+          border: InputBorder.none,
+          prefixIcon: const Icon(
+            Icons.search_rounded,
+            color: AppColors.mutedText,
+            size: 22,
+          ),
+        ),
+      ),
+    );
+  }
+}
