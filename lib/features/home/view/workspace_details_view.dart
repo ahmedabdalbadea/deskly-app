c09a312d9389cdfb55e30baa1968e026759a515a
Ahmed Abd Albadea <ahmedmohmaedabdalbadea@gmail.com>
2026-09-13 15:47:54 +0300

    add book now button

--- lib/features/home/view/workspace_details_view.dart
+++ lib/features/home/view/workspace_details_view.dart
@@ -1,3 +1,4 @@
+import 'package:deskly_app/core/widget/gradient_button.dart';
 import 'package:flutter/material.dart';
 
 import 'widget/workspace_details_body.dart';
@@ -7,8 +8,19 @@ class WorkspaceDetailsView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: SafeArea(top: false, child: WorkspaceDetailsBody()),
+    return Scaffold(
+      body: const SafeArea(top: false, child: WorkspaceDetailsBody()),
+      bottomNavigationBar: Padding(
+        padding: const EdgeInsets.only(left: 20, right: 20, bottom: 24),
+        child: SizedBox(
+          height: 60,
+          child: GradientButton(
+            title: "Book Now",
+            radius: 14,
+            padding: EdgeInsets.symmetric(vertical: 12),
+          ),
+        ),
+      ),
     );
   }
 }
