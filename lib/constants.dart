46fd79752ba215194a2ca67294960f73f7a1e3fd
Ahmed Abd Albadea <ahmedmohmaedabdalbadea@gmail.com>
2026-09-19 15:58:19 +0300

    created the constants needed for the data sources

--- lib/constants.dart
+++ lib/constants.dart
@@ -2,3 +2,12 @@ const kAnimationDuration = Duration(milliseconds: 320);
 const kPressAnimationDuration = Duration(milliseconds: 100);
 const kTransitionDuration = Duration(milliseconds: 500);
 const kUsersCollection = 'users';
+const kCategoriesCollection = 'categories';
+const kWorkspacesCollection = 'workspaces';
+const kWorspacesCategoryIdField = 'categoryId';
+const kWorkspacesRatingField = "rating";
+const kPopularWorkspacesBox = 'popularWorkspaces';
+const kNearbyWorkspacesBox = 'nearbyWorkspaces';
+const kCategoriesBox = 'categoriesCache';
+const kUserBox = 'user';
+
