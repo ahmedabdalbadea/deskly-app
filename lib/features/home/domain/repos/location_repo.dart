b0bf0274120a439c15aa7181dfb8e48310c3970e
Ahmed Abd Albadea <ahmedmohmaedabdalbadea@gmail.com>
2026-09-20 15:17:45 +0300

    create location repo

---
+++ lib/features/home/domain/repos/location_repo.dart
@@ -0,0 +1,11 @@
+import 'package:dartz/dartz.dart';
+import 'package:deskly_app/core/errors/failure.dart';
+import 'package:deskly_app/features/home/domain/entity/location_entity.dart';
+import 'package:deskly_app/features/home/domain/enums/location_permission_status.dart';
+
+abstract class LocationRepo {
+  Future<bool> isLocationServiceEnabled();
+  Future<LocationPermissionStatus> checkPermission();
+  Future<LocationPermissionStatus> requestPermission();
+  Future<Either<Failure, LocationEntity>> fetchCurrentLocation();
+}
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -10,6 +10,7 @@ import facebook_auth_desktop
 import firebase_auth
 import firebase_core
 import flutter_secure_storage_darwin
+import geocoding_darwin
 import geolocator_apple
 import google_sign_in_ios
 import package_info_plus
@@ -20,6 +21,7 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FlutterSecureStorageDarwinPlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStorageDarwinPlugin"))
+  GeocodingDarwinPlugin.register(with: registry.registrar(forPlugin: "GeocodingDarwinPlugin"))
   GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
   FLTGoogleSignInPlugin.register(with: registry.registrar(forPlugin: "FLTGoogleSignInPlugin"))
   FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
