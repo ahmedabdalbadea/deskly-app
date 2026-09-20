0ca54a3f7d86b350f5b12419eecdd65e5ae7e45c
Ahmed Abd Albadea <ahmedmohmaedabdalbadea@gmail.com>
2026-09-20 18:23:17 +0300

    edit location failure

--- lib/features/home/domain/error/location_failure.dart
+++ lib/features/home/domain/error/location_failure.dart
@@ -6,12 +6,16 @@ class LocationFailure extends Failure {
 
   LocationFailure(this.status) : super(_messageFor(status));
 
-  static String _messageFor(LocationPermissionStatus status) => switch (status) {
-    LocationPermissionStatus.serviceDisabled => 'Location service is disabled',
-    LocationPermissionStatus.denied => 'Location permission denied',
+  static String _messageFor(
+    LocationPermissionStatus status,
+  ) => switch (status) {
+    LocationPermissionStatus.serviceDisabled =>
+      'Location services are turned off. Please enable GPS from your device settings.',
+    LocationPermissionStatus.denied =>
+      'Location permission was denied. Please allow location access to see workspaces near you.',
     LocationPermissionStatus.deniedForever =>
-      'Location permission permanently denied',
-    LocationPermissionStatus.granted => 'Could not get location',
-    LocationPermissionStatus.unknown => 'Could not get location',
+      'Location permission is permanently denied. Please enable it for Deskly from your app settings.',
+    LocationPermissionStatus.granted || LocationPermissionStatus.unknown =>
+      'Unable to get your location. Please try again later.',
   };
 }
