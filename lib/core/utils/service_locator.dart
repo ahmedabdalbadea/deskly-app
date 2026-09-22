6eda70b986c7857e65c5eab2e5279d1bdf824885
Ahmed Abd Albadea <ahmedmohmaedabdalbadea@gmail.com>
2026-09-22 18:30:38 +0300

    edit service loactor and provider location and workspaces adn address cubit

--- lib/core/utils/service_locator.dart
+++ lib/core/utils/service_locator.dart
@@ -7,8 +7,21 @@ import 'package:deskly_app/features/auth/domain/use_case/register/register_use_c
 import 'package:deskly_app/features/auth/domain/use_case/reset_password_use_case.dart';
 import 'package:deskly_app/features/auth/domain/use_case/social_auth_use_case.dart';
 import 'package:deskly_app/features/auth/manager/auth_cubit/auth_cubit.dart';
+import 'package:deskly_app/features/home/data/data%20source/home_local_data_source.dart';
+import 'package:deskly_app/features/home/data/data%20source/home_remote_data_source.dart';
+import 'package:deskly_app/features/home/data/repos/home_repo_impl.dart';
+import 'package:deskly_app/features/home/data/repos/location_repo_impl.dart';
+import 'package:deskly_app/features/home/domain/repos/home_repo.dart';
+import 'package:deskly_app/features/home/domain/repos/location_repo.dart';
+import 'package:deskly_app/features/home/domain/use%20case/fetch_address_from_location.dart';
+import 'package:deskly_app/features/home/domain/use%20case/fetch_current_location_use_case.dart';
+import 'package:deskly_app/features/home/domain/use%20case/fetch_popular_workspaces_use_case.dart';
+import 'package:deskly_app/features/home/manager/address_cubit/address_cubit.dart';
+import 'package:deskly_app/features/home/manager/location_cubit/location_cubit.dart';
+import 'package:deskly_app/features/home/manager/workspaces_cubit/workspaces_cubit.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_facebook_auth/flutter_facebook_auth.dart';
+import 'package:geocoding/geocoding.dart';
 import 'package:get_it/get_it.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 
@@ -21,7 +34,9 @@ Future<void> setupServiceLocator() async {
   );
   getIt.registerLazySingleton<GoogleSignIn>(() => GoogleSignIn.instance);
   getIt.registerLazySingleton<FacebookAuth>(() => FacebookAuth.instance);
+  getIt.registerLazySingleton<Geocoding>(() => Geocoding());
 
+  // Auth Feature
   getIt.registerLazySingleton<AuthRemoteDataSource>(
     () => AuthRemoteDataSource(
       firebaseAuth: getIt(),
@@ -47,6 +62,51 @@ Future<void> setupServiceLocator() async {
       socialAuthUseCase: getIt(),
       resetPasswordUseCase: getIt(),
     ),
+  );
+
+  // Home Feature Data & Repos
+  getIt.registerLazySingleton<HomeRemoteDataSource>(
+    () => HomeRemoteDataSource(
+      auth: getIt(),
+      firestore: getIt(),
+    ),
+  );
+
+  getIt.registerLazySingleton<HomeLocalDataSource>(
+    () => HomeLocalDataSource(),
+  );
+
+  getIt.registerLazySingleton<HomeRepo>(
+    () => HomeRepoImpl(
+      remoteDataSource: getIt(),
+      localDataSource: getIt(),
+    ),
+  );
+
+  getIt.registerLazySingleton<LocationRepo>(
+    () => LocationRepoImpl(geocoding: getIt()),
+  );
+
+  // Home Feature Use Cases
+  getIt.registerLazySingleton(
+    () => FetchCurrentLocationUseCase(locationRepo: getIt()),
+  );
+  getIt.registerLazySingleton(
+    () => FetchPopularWorkspacesUseCase(homeRepo: getIt()),
+  );
+  getIt.registerLazySingleton(
+    () => FetchAddressFromLocation(locationRepo: getIt()),
+  );
+
+  // Home Feature Cubits
+  getIt.registerFactory(
+    () => LocationCubit(fetchCurrentLocationUseCase: getIt()),
+  );
+  getIt.registerFactory(
+    () => WorkspacesCubit(fetchPopularWorkspacesUseCase: getIt()),
+  );
+  getIt.registerFactory(
+    () => AddressCubit(fetchAddressUseCase: getIt()),
   );
 
   await getIt<AuthRemoteDataSource>().initializeGoogleSignIn();
--- lib/features/home/view/home_view.dart
+++ lib/features/home/view/home_view.dart
@@ -1,6 +1,7 @@
-import 'package:deskly_app/features/home/data/repos/location_repo_impl.dart';
-import 'package:deskly_app/features/home/domain/use%20case/fetch_current_location_use_case.dart';
+import 'package:deskly_app/core/utils/service_locator.dart';
+import 'package:deskly_app/features/home/manager/address_cubit/address_cubit.dart';
 import 'package:deskly_app/features/home/manager/location_cubit/location_cubit.dart';
+import 'package:deskly_app/features/home/manager/workspaces_cubit/workspaces_cubit.dart';
 import 'package:deskly_app/features/home/view/widget/home_view_body_bloc_builder.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -13,12 +14,20 @@ class HomeView extends StatelessWidget {
     return Scaffold(
       body: SafeArea(
         top: false,
-        child: BlocProvider(
-          create: (context) => LocationCubit(
-            fetchCurrentLocationUseCase: FetchCurrentLocationUseCase(
-              locationRepo: LocationRepoImpl(),
+        child: MultiBlocProvider(
+          providers: [
+            BlocProvider(
+              create: (context) =>
+                  getIt<LocationCubit>()..checkLocationReadiness(),
             ),
-          )..checkLocationReadiness(),
+            BlocProvider(
+              create: (context) =>
+                  getIt<WorkspacesCubit>()..fetchPopularWordspaces(),
+            ),
+            BlocProvider(
+              create: (context) => getIt<AddressCubit>(),
+            ),
+          ],
           child: const HomeViewBodyBlocBuilder(),
         ),
       ),
